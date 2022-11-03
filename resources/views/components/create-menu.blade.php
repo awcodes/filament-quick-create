9830ec04c551f5657a36ac04960e9c9532420fb1
awcodes <adam@adamwgd.com>
2022-11-03 17:09:21 -0400

    working wip

--- resources/views/components/create-menu.blade.php
+++ resources/views/components/create-menu.blade.php
@@ -26,5 +26,62 @@
             @endforeach
         </ul>
     </x-filament::dropdown>
+
+        <form wire:submit.prevent="callMountedAction">
+            @php
+                $action = $this->getMountedAction();
+            @endphp
+
+            <x-filament::modal
+                    id="page-action"
+                    :wire:key="$action ? $this->id . '.actions.' . $action->getName() . '.modal' : null"
+                    :visible="filled($action)"
+                    :width="$action?->getModalWidth()"
+                    :slide-over="$action?->isModalSlideOver()"
+                    display-classes="block"
+            >
+                @if ($action)
+                    @if ($action->isModalCentered())
+                        <x-slot name="heading">
+                            {{ $action->getModalHeading() }}
+                        </x-slot>
+
+                        @if ($subheading = $action->getModalSubheading())
+                            <x-slot name="subheading">
+                                {{ $subheading }}
+                            </x-slot>
+                        @endif
+                    @else
+                        <x-slot name="header">
+                            <x-filament::modal.heading>
+                                {{ $action->getModalHeading() }}
+                            </x-filament::modal.heading>
+
+                            @if ($subheading = $action->getModalSubheading())
+                                <x-filament::modal.subheading>
+                                    {{ $subheading }}
+                                </x-filament::modal.subheading>
+                            @endif
+                        </x-slot>
+                    @endif
+
+                    {{ $action->getModalContent() }}
+
+                    @if ($action->hasFormSchema())
+                        {{ $this->getMountedActionForm() }}
+                    @endif
+
+                    @if (count($action->getModalActions()))
+                        <x-slot name="footer">
+                            <x-filament::modal.actions :full-width="$action->isModalCentered()">
+                                @foreach ($action->getModalActions() as $modalAction)
+                                    {{ $modalAction }}
+                                @endforeach
+                            </x-filament::modal.actions>
+                        </x-slot>
+                    @endif
+                @endif
+            </x-filament::modal>
+        </form>
 @endif
 </div>
