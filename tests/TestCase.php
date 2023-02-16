1aaef1b4396d891bf6815066411d87b84416a72f
awcodes <adam@adamwgd.com>
2023-02-16 09:49:49 -0500

    Chore: Laravel 10 support and workflow updates

---
+++ tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FilamentQuickCreate\Tests;
+
+use FilamentQuickCreate\FilamentQuickCreateServiceProvider;
+use Filament\FilamentServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Livewire\LivewireServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Awcodes\\FilamentExtras\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LivewireServiceProvider::class,
+            FilamentServiceProvider::class,
+            FilamentQuickCreateServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_filament-extras_table.php.stub';
+        $migration->up();
+        */
+    }
+}
