d2841a9108381a7b37c422c45f8ad6b19764bf7d
Adam Weston <adam@awcodes.com>
2026-05-18 09:45:34 -0400

    Fix: cachedMountedActions

--- rector.php
+++ rector.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__.'/src',
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    )
-    ->withPhpSets();
+try {
+    return RectorConfig::configure()
+        ->withPaths([
+            __DIR__.'/src',
+        ])
+        ->withPreparedSets(
+            deadCode: true,
+            codeQuality: true,
+            typeDeclarations: true,
+            privatization: true,
+            earlyReturn: true,
+        )
+        ->withPhpSets();
+} catch (Rector\Exception\Configuration\InvalidConfigurationException $e) {
+    echo 'Rector configuration error: '.$e->getMessage().PHP_EOL;
+    exit(1);
+}
